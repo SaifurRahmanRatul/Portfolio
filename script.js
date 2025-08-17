0a7ec1bcf3e8209a63b6742b717918096ce61b10
Md. Saifur Rahman <saifurrahmanratul19@gmail.com>
2025-08-17 22:45:13 +0600

    Added conditionals and loops to JS

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ const startYear = 2023;
 let experienceYears = currentYear - startYear;
 console.log("Years of Experience: " + experienceYears);
 
+// Conditional check
+if (experienceYears > 1) {
+    console.log(fullName + " is an Experienced Developer!");
+} else {
+    console.log(fullName + " is a Junior Developer!");
+}
+
+// Loop through skills
+const skills = ["HTML5", "CSS3", "JavaScript", "React"];
+for (let i = 0; i < skills.length; i++) {
+    console.log("Skill " + (i + 1) + ": " + skills[i]);
+}
+
 // Show welcome alert on page load
 window.addEventListener('load', () => {
     alert("Welcome to my portfolio! I'm " + fullName + " with " + experienceYears + " years in tech.");
